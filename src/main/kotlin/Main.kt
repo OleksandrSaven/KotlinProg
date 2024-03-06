559f0aa764853f6ac2b9e1380cb8c923d9b03e9c
Oleksandr Savenets <134528120+OleksandrSaven@users.noreply.github.com>
2024-03-06 16:38:00 +0200

    separete logic, add check NotBlank

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
 import java.util.Scanner
 
 fun main() {
+    scanner()
+}
+fun scanner() {
     val scanner = Scanner(System.`in`)
     print("Enter you city... ")
-    val city = scanner.nextLine()
-    println("User lives in $city")
+    val city = readln()
+    if(city.isNotBlank()) {
+        println("User lives in $city")
+    }
     scanner.close()
 }
